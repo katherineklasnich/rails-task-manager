2aa1dd1a9ce32b9a78bb41393af7df76722005e7
Katherine Klasnich <katherineklasnich@gmail.com>
2019-05-10 16:47:46 +0100

    all done on rails task manager challenge EXCEPT delete task

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # home page of tasks
+  get "tasks", to: "tasks#index"
+
+  # create a new task
+  get "tasks/new", to: "tasks#new"
+
+  # navigate to one task
+  get "tasks/:id", to: "tasks#show", as: "task"
+
+  # create a new task
+  get "tasks/new", to: "tasks#new"
+
+  # request to submit new task
+  post "tasks", to: "tasks#create"
+
+  # get info to update a task
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit"
+
+  #push updated info into task
+  patch "tasks/:id", to: "tasks#update", as: "update"
 end
